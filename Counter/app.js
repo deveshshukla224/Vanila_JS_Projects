3889594481ea12ed942dc0e0d0d76aed1fe692ea
unknown <devesh224.cavisson@gmail.com>
2021-04-22 11:38:35 +0530

    counter logic

---
+++ Counter/app.js
@@ -0,0 +1,36 @@
+//initial count value
+let count = 0;
+//select value of counter and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn){
+             btn.addEventListener("click",function(event){
+              const styles = event.currentTarget.classList;
+              if(styles.contains("decrease"))
+                {
+                 count--;
+                }
+                 else if(styles.contains("increase"))
+                  {
+                   count++; 
+                  }
+                else{
+                  count = 0;
+                 }
+                if(count>0)
+                {
+                 value.style.color="green";
+                }
+                if(count<0)
+                {
+                value.style.color="red";
+                }
+                if(count == 0)
+                {
+                value.style.color="#222";
+                }
+                value.textContent=count;
+                })
+                });
+
